8de69b27834f2bf7f802d31e46662893a8da204f
jjangchan <rudcks6806@gamil.com>
2021-09-01 09:18:07 +0900

    modify gitignore

--- baekjoon/1786.h
+++ baekjoon/1786.h
@@ -1,5 +1,6 @@
 //
 // Created by jjangchan on 2021-08-25.
+// 1786 - 찾기(KMP)
 //
 
 #ifndef ALGORITHM_1786_H
@@ -9,8 +10,50 @@ vector<int> getPi(string p)
 {
     int m = (int)p.size(), j=0;
     vector<int> pi(m, 0);
-    for(int i = 1; i< m ; i++){
-        while(j > 0 && p[i] != p[j]) j = pi[j-1]; if(p[i] == p[j]) pi[i] = ++j; } return pi; } vector<int> kmp(string s, string p){ vector<int> ans; auto pi = getPi(p); int n = (int)s.size(), m = (int)p.size(), j =0; for(int i = 0 ; i < n ; i++){ while(j>0 && s[i] != p[j]) j = pi[j-1]; if(s[i] == p[j]){ if(j==m-1){ ans.push_back(i-m+1); j = pi[j]; }else{ j++; } } } return ans; }
+    for(int i = 1; i< m ; i++)
+    {
+        while(j > 0 && p[i] != p[j])
+            j = pi[j-1];
+        if(p[i] == p[j])
+            pi[i] = ++j;
+    }
+    return pi;
+}
 
+
+
+vector<int>kmp(string s, string p){
+    vector<int> ans;
+    auto pi = getPi(p);
+    int n = (int)s.size(), m = (int)p.size(), j =0;
+    for(int i = 0 ; i < n ; i++)
+    {
+        while(j>0 && s[i] != p[j]) j = pi[j-1];
+        if(s[i] == p[j])
+        {
+            if(j==m-1)
+            {
+                ans.push_back(i-m+1);
+                j = pi[j];
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+    return ans;
+}
+
+void Input()
+{
+    string s, p;
+    getline(cin, s);
+    getline(cin, p);
+    auto matched = kmp(s,p);
+    printf("%d\n", (int)matched.size());
+    for(auto i : matched)
+        printf("%d ", i+1);
+}
 
 #endif //ALGORITHM_1786_H
--- main.cpp
+++ main.cpp
@@ -6,43 +6,6 @@
 #include <unordered_map>
 #include "programmers/Ex3)2021KakaoBlind.h"
 
-
-const string ALL = "-";
-unordered_map<string, vector<int>> map;
-
-void insert(string* key, int mask, int point) {
-    string s = "";
-    for (int i = 0; i < 4; i++) {
-        s += (mask & (1 << i)) ? ALL : key[i];
-        map[s].push_back(point);
-    }
-}
-
-vector<int> Solution(vector<string> info, vector<string> query) {
-    vector<int> answer;
-    string key[4], tmp;
-    int point;
-
-    for (auto& inf : info) {
-        istringstream iss(inf);
-        iss >> key[0] >> key[1] >> key[2] >> key[3] >> point;
-        for (int i = 0; i < 16; i++) insert(key, i, point);
-    }
-
-    for (auto& m : map) sort(m.second.begin(), m.second.end());
-
-    for (auto& que : query) {
-        istringstream iss(que);
-        iss >> key[0] >> tmp >> key[1] >> tmp >> key[2] >> tmp >> key[3] >>
-            point;
-        string s = key[0] + key[1] + key[2] + key[3];
-        vector<int>& v = map[s];
-        answer.push_back(v.end() - lower_bound(v.begin(), v.end(), point));
-    }
-
-    return answer;
-}
-
 int main()
 {
     std::vector<std::string> info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
