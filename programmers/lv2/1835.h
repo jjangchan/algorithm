bd0b41828121c9f141b78e0f1130c5ef9758b469
jjangchan <rudcks6806@gamil.com>
2021-12-27 10:09:43 +0900

    merge

--- programmers/lv2/1835.h
+++ programmers/lv2/1835.h
@@ -11,14 +11,6 @@
 
 using namespace std;
 
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
-// 순열 경우의수 적어도 문제
-// 전체! - (여사건) = 답
-// 여사건은 case 별로 나눠보자
-int solution(int n, vector<string> data) {
-    int answer = 0;
-    return answer;
-}
 
 
 #endif //MAIN_CPP_1835_H
