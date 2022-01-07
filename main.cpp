bf6515088137b3c200bfff861a66433bf1a190c3
jjangchan <rudcks6806@gmail.com>
2022-01-08 06:51:20 +0900

    프로그래머스 lv2 푸는중

--- main.cpp
+++ main.cpp
@@ -3,20 +3,6 @@
 #include "programmers/lv2/67257.h"
 
 
-void permutaion(bool* is, const std::string& str, const std::string& target){
-    if(target.size() == str.size()){
-        std::cout << target << std::endl;
-    }
-
-    for(int i = 0; i < str.size(); i++){
-        if(!is[i]){
-            is[i] = true;
-            permutaion(is, str, target+str[i]);
-            is[i] = false;
-        }
-    }
-}
-
 int main() {
     solution("100-200*300-500+20");
     return 0;
--- programmers/lv2/67257.h
+++ programmers/lv2/67257.h
@@ -15,11 +15,57 @@
 
 using namespace std;
 
+long long answer;
+
+long long Calculate(const long long a, const long long b, char c){
+    switch (c) {
+        case '*' : return a*b;
+        case '-' : return a-b;
+        case '+' : return a+b;
+    }
+}
+
+void Calculate(const std::vector<std::string> &v){
+    std::stack<long long> s;
+    for(const auto& str : v){
+        if(!isdigit(str[0])){
+            long long b = s.top();
+            s.pop();
+            long long a = s.top();
+            s.pop();
+            s.push(Calculate(a, b, str[0]));
+        }else s.push(std::stoll(str));
+    }
+    long long result = std::abs(s.top());
+    answer = result > answer ? result : answer;
+}
+
+void ParseOperator(const std::vector<std::string>& v, std::map<std::string, int>& priority){
+    // 중의표기법 -> 후의표기법
+    std::stack<std::string> s;
+    std::vector<std::string> transform;
+    for(const auto& str : v){
+        if(!isdigit(str[0])){
+            while(!s.empty() && priority[s.top()] >= priority[str]) {
+                transform.push_back(s.top());
+                s.pop();
+            }
+            s.push(str);
+        }else transform.push_back(str);
+    }
+    while(!s.empty()){
+        transform.push_back(s.top());
+        s.pop();
+    }
+    Calculate(transform);
+}
+
 void Permutation(const std::string& str, const std::string& target, bool *is, const std::vector<std::string> &v){
     if(str.size() == target.size()){
         std::map<std::string, int> priority;
-        for(int i = 0; i < target.size(); i++) priority[target.substr(i)] = i;
-        
+        for(int i = 0; i < target.size(); i++) priority[target.substr(i,1)] = target.size()-i;
+        ParseOperator(v, priority);
+        return;
     } else for(int i = 0; i < str.size(); i++){
         if(!is[i]){
             is[i] = true;
@@ -47,11 +93,12 @@ void StrSplit(std::vector<std::string> &splited, std::set<std::string>& s, const
 }
 
 long long solution(string expression) {
-    long long answer = 0;
+    answer = 0;
     std::vector<std::string> splited;
     std::set<std::string> s;
     StrSplit(splited, s, expression);
-    std::string ope(s.begin(), s.end());
+    std::string ope ="";
+    for(const auto a : s) ope += a;
     bool is[ope.size()];
     memset(is, false, ope.size());
     Permutation(ope, "", is, splited);
